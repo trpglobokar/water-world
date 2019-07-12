51dc0bda7297738590f6bcd7ff9359d61514b0f3
Rocky PG <trp.globokar@gmail.com>
2019-07-12 00:31:55 -0700

    feat: added basic world topo map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import L from 'leaflet'
+import topoContors from './topocontours.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function coolStyle(feature) {
+  const elevation = feature.properties.elevation
+
+  const fillColor = elevation < 800 ? "#aad3df" : "#060"
+  const fillOpacity = elevation < 800 ? 1 : 0.3
+  const weight = elevation === 0 ? 1 : 0
+  return {
+      fillColor,
+      weight,
+      opacity: 0.3,
+      color: "#fff",
+      fillOpacity
+  }
 }
 
-export default App;
+export default class App extends Component {
+  componentDidMount() {
+    // create map
+    let map = L.map('map', {
+      center: [0, 0],
+      zoom: 2
+    });
+
+    /*L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)*/
+
+    L.geoJson(topoContors, {style: coolStyle}).addTo(map)
+  }
+
+  render() {
+    return (
+      <div id="map" style={{height: "100vh", backgroundColor: "#aad3df"}}></div>
+    )
+  }
+}
