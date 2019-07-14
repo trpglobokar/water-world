520594b9ece9a1f9905f90d60150a0ce6df1405d
Rocky PG <trp.globokar@gmail.com>
2019-07-13 21:43:35 -0700

    Updates

---
+++ precache-manifest.ca1db98832addf3fe7489e7ed4fbfdfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efbcd72c211d4d9236b74459be7eceb",
+    "url": "/water-world/index.html"
+  },
+  {
+    "revision": "6381686545849ea482bc",
+    "url": "/water-world/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3fcb35f91cfa7cdfba26",
+    "url": "/water-world/static/js/2.7fdb500f.chunk.js"
+  },
+  {
+    "revision": "6381686545849ea482bc",
+    "url": "/water-world/static/js/main.3c9d972e.chunk.js"
+  },
+  {
+    "revision": "465398af25ddedd32ee3",
+    "url": "/water-world/static/js/runtime~main.46eb4481.js"
+  }
+]);
